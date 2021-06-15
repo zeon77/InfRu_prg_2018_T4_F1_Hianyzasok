b005b41dd394219797687820d5c44fac4c19c2cf
zeon77 <zeon77@gmail.com>
2021-06-15 02:09:28 +0200

    2. feladat

--- Hianyzasok/Program.cs
+++ Hianyzasok/Program.cs
@@ -14,7 +14,11 @@ namespace Hianyzasok
             List<Hianyzas> hianyzasok = new List<Hianyzas>();
             foreach (var sor in File.ReadAllLines(@"szeptember.csv", Encoding.Default).Skip(1))
                 hianyzasok.Add(new Hianyzas(sor));
-            
+
+            //2. feladat
+            Console.WriteLine("2. feladat:");
+            Console.WriteLine($"\tÖsszes mulasztott órák száma: {hianyzasok.Sum(h => h.MulasztottÓrák)} óra.");
+
         }
     }
 }
